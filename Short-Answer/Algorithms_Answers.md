eaa6ec0471ed5138184b6b96b61447d4adab2abc
Uzias Rivera <uziasrivera@gmail.com>
2020-02-21 08:55:36 -0800

    completed recursion

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -12,4 +12,7 @@ c)
 
 ## Exercise II
 
+The first approach I would rule is a linear one. This would require looking through an entire set of floors before determining the value of f and thus costing too many eggs. I would take the approach of  dropping the egg from the middle of the building and seeing if it breaks. If it does, then I would no longer need to check upper half of the building.
+    
+
 
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,21 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    #takes in string, returns number    
+    # print(word)
+    # print(word)
+    if word == 'th':
+        return 1
+    elif len(word) == 2 and word != 'th':
+        return 0
+    elif len(word)==0:
+        return 0
+    elif len(word)==1:
+        return 0
+    else:
+        half = len(word)//2
+        # print(half)
+        first_half = word[:half]
+        second_half = word[half:]
+        count =  count_th(first_half) + count_th(second_half)
+        return  count + count_th(word[half-1]+word[half])
